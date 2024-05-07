52f0517c2ea9a2d332ab06340142dd04c26808d0
HanmaDevin <devin12@icloud.com>
2024-05-07 06:39:13 +0200

    fixed wrong cloning with zsh-plugins

--- after-linux-installation.sh
+++ after-linux-installation.sh
@@ -84,9 +84,10 @@ if [[ $zshplugins == "y" ]]; then
     if [[ -d "~/zsh-plugins" ]]; then
         rm -r ~/zsh-plugins
         mkdir -p ~/zsh-plugins
-        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zsh-plugins 
+        cd ~/zsh-plugins
+        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
         echo "source ~/zsh-plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
-        git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/zsh-plugins
+        git clone https://github.com/zsh-users/zsh-autosuggestions.git
         echo "source ~/zsh-plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
     fi
 fi
