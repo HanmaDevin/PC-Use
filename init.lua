93e31a7ce38b4848726783c53a98db52202490d7
HanmaDevin <devin12@icloud.com>
2024-05-23 00:02:48 +0200

    seperated all plugins

--- init.lua
+++ init.lua
@@ -20,16 +20,3 @@ vim.opt.rtp:prepend(lazypath)
 local opts = {}
 
 require("lazy").setup("plugins")
-local builtin = require("telescope.builtin")
-
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left', {})
-
-local config = require("nvim-treesitter.configs")
-config.setup({
-  ensure_installed = {"lua", "javascript", "python", "c", "java"},
-  highlight = {enable = true}, 
-  indent = {enable = true},
-})
