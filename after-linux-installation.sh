5113cb9167ddb94580e3e40a4e161f7ad0ea6d21
HanmaDevin <>
2024-06-10 21:39:15 +0200

    seperated zsh theme

--- after-linux-installation.sh
+++ after-linux-installation.sh
@@ -70,9 +70,6 @@ if [[ $ohmyzsh == "y" ]]; then
     else 
         sudo apt install zsh
     fi
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-    echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 fi
 
 read -p "Do you want to install zsh-plugins (y/n)" zshplugins
