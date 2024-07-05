b53384098bea1bb803abd3c1bd0ae17ffeb43fc2
HanmaDevin <devinbc12@icloud.com>
2024-07-05 11:30:39 +0200

    removed neogit, not needed

--- neovim.sh
+++ neovim.sh
@@ -7,13 +7,13 @@ echo "Welcome to my neovim configuration script"
 read -p "Want to install neovim? (y/n)" neovim
 
 if [[ $neovim == "y" ]]; then
-	if [[ $ID == "fedora" ]]; then
-		sudo dnf install neovim
-	elif [[ $ID == "arch" ]]; then
-		sudo pacman -S neovim
-	else
-		sudo apt install neovim
-	fi
+  if [[ $ID == "fedora" ]]; then
+    sudo dnf install neovim
+  elif [[ $ID == "arch" ]]; then
+    sudo pacman -S neovim
+  else
+    sudo apt install neovim
+  fi
 fi
 
 cd ~/.config/
@@ -24,6 +24,3 @@ sleep 1
 
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
-
-cd ~/.config/nvim/lua/plugins/
-curl -O https://raw.githubusercontent.com/HanmaDevin/NeoVim/main/neogit.lua
