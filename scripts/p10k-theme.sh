d00261eb861ed4034effc1072e46af66893b83cd
HanmaDevin <devin.cardosa@proton.me>
2024-07-20 01:04:02 +0200

    updated stuff for eos the best distro and ootb i3 exp

--- scripts/p10k-theme.sh
+++ scripts/p10k-theme.sh
@@ -6,7 +6,7 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo "Adding zshrc"
 sleep 2
 
-cp "$HOME/Linux/dotfiles/zshrc" "$HOME/.zshrc"
+cp "$HOME/Linux/zsh/zshrc" "$HOME/.zshrc"
 
 echo "Finished!"
 sleep 2
