b5f4c62344d288d233494e1d39f11eeb989f6a2d
HanmaDevin <devin.cardosa@proton.me>
2024-07-27 07:00:21 +0200

    Update p10k-theme.sh

--- scripts/p10k-theme.sh
+++ scripts/p10k-theme.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 
-echo "Adding zshrc"
-sleep 2
-
-cp "$HOME/Linux/zsh/zshrc" "$HOME/.zshrc"
-
-echo "Finished!"
-sleep 2
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
