7261b691fb40f07df9e40a4eedc900834e08754e
HanmaDevin <devin.cardosa@proton.me>
2024-10-24 21:12:03 +0200

    updated wallpaper script

--- scripts/get-wallpaper.sh
+++ scripts/get-wallpaper.sh
@@ -3,10 +3,16 @@
 # installs wallpaper
 if [[ ! -d "$HOME/Linux"]]; then
     git clone "https://github.com/HanmaDevin/Linux.git" "$HOME/Linux"
+
+else 
+    exit 123
 fi
 
 if [[ ! -d "$HOME/Pictures/wallpapers" ]]; then
     mkdir -p "$HOME/Pictures/wallpapers"
+
+else
+    exit 123
 fi
 
 echo "Installing wallpapers..."
