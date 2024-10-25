9de3d3fb94ed7585538a20bc50024b5ea3467198
HanmaDevin <devin.cardosa@proton.me>
2024-10-26 01:37:53 +0200

    Add support for zypper and pacman package managers

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,6 +3,8 @@
 echo "What package manager do you use?"
 echo "(1) apt"
 echo "(2) dnf"
+echo "(3) zypper"
+echo "(4) pacman"
 
 read answer
 
@@ -169,6 +171,140 @@ case $answer in
       echo "adding fish config"
 
       cp "$HOME/Linux/fish/fedora.fish" "$HOME/.config/fish/config.fish"
+
+      echo "Finished!"
+    fi
+    ;;
+  3)
+    install_packages "zypper" "zip" "zsh" "fish" "unzip" "wget" "curl" "neovim" "eza" "neofetch" "btop" "gamemode" "mangohud" "zoxide" "fzf" "bat" "kitty"
+    configure_git
+    add_ssh_key
+    
+    echo "Adding btop config"
+
+    if [[ ! -d "$HOME/.config/btop" ]]; then
+        mkdir -p "$HOME/.config/btop"
+    fi
+
+    cp "$HOME/Linux/btop/btop.conf" "$HOME/.config/btop"
+
+    echo "Finished!"
+
+    echo "Adding kitty config"
+
+    if [[ ! -d "$HOME/.config/kitty" ]]; then
+        mkdir -p "$HOME/.config/kitty"
+    fi
+
+    cp -a "$HOME/Linux/kitty/." "$HOME/.config/kitty"
+
+    echo "Finished!"
+
+    # adding neovim plugin
+    echo "Adding neovim plugin"
+    echo "Running neovim script"
+
+    bash "$HOME/Linux/scripts/neovim.sh"
+
+    if [[ ! -d "$HOME/.config/nvim/lua/plugins/" ]]; then
+      mkdir -p "$HOME/.config/nvim/lua/plugins/"
+    fi
+
+    cp "$HOME/Linux/neovim/lazygit.lua" "$HOME/.config/nvim/lua/plugins/"
+
+    echo "Finished!"
+
+    add_wallpaper
+
+    echo "Adding font and cursor"
+
+    sudo cp "$HOME/Linux/Fonts/MesloLGS NF Regular.ttf" "/usr/share/fonts/"
+    sudo cp -r "$HOME/Linux/Cursor/Bibata-Modern-Ice/" "/usr/share/icons/"
+
+    echo "Finished!"
+
+    read -p "Use (1) fish or (2) zsh? " shell
+    if [[ "$shell" -eq 2 ]]; then
+      echo "adding zshrc"
+
+      bash "$HOME/Linux/scripts/p10k-theme.sh"
+      cp "$HOME/Linux/zsh/debianzshrc" "$HOME/.zshrc"
+
+      echo "Finished!"
+
+    elif [[ "$shell" -eq 1 ]]; then
+      echo "adding fish config"
+
+      cp "$HOME/Linux/fish/debian.fish" "$HOME/.config/fish/config.fish"
+
+      echo "Finished!"
+    fi
+    ;;
+  4)
+    packages=("zip" "zsh" "fish" "unzip" "wget" "curl" "neovim" "eza" "neofetch" "btop" "gamemode" "mangohud" "zoxide" "fzf" "bat" "kitty")
+    for package in "${packages[@]}"; do
+      sudo pacman -S --no-confirm "$package" || { echo "Failed to install $package"; exit 1; }
+    done
+
+    configure_git
+    add_ssh_key
+    
+    echo "Adding btop config"
+
+    if [[ ! -d "$HOME/.config/btop" ]]; then
+        mkdir -p "$HOME/.config/btop"
+    fi
+
+    cp "$HOME/Linux/btop/btop.conf" "$HOME/.config/btop"
+
+    echo "Finished!"
+
+    echo "Adding kitty config"
+
+    if [[ ! -d "$HOME/.config/kitty" ]]; then
+        mkdir -p "$HOME/.config/kitty"
+    fi
+
+    cp -a "$HOME/Linux/kitty/." "$HOME/.config/kitty"
+
+    echo "Finished!"
+
+    # adding neovim plugin
+    echo "Adding neovim plugin"
+    echo "Running neovim script"
+
+    bash "$HOME/Linux/scripts/neovim.sh"
+
+    if [[ ! -d "$HOME/.config/nvim/lua/plugins/" ]]; then
+      mkdir -p "$HOME/.config/nvim/lua/plugins/"
+    fi
+
+    cp "$HOME/Linux/neovim/lazygit.lua" "$HOME/.config/nvim/lua/plugins/"
+
+    echo "Finished!"
+
+    add_wallpaper
+
+    echo "Adding font and cursor"
+
+    sudo cp "$HOME/Linux/Fonts/MesloLGS NF Regular.ttf" "/usr/share/fonts/"
+    sudo cp -r "$HOME/Linux/Cursor/Bibata-Modern-Ice/" "/usr/share/icons/"
+
+    echo "Finished!"
+
+    read -p "Use (1) fish or (2) zsh? " shell
+    if [[ "$shell" -eq 2 ]]; then
+      echo "adding zshrc"
+
+      bash "$HOME/Linux/scripts/p10k-theme.sh"
+      cp "$HOME/Linux/zsh/debianzshrc" "$HOME/.zshrc"
+
+      echo "Finished!"
+
+    elif [[ "$shell" -eq 1 ]]; then
+      echo "adding fish config"
+
+      cp "$HOME/Linux/fish/debian.fish" "$HOME/.config/fish/config.fish"
 
       echo "Finished!"
     fi
