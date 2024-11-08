7f58629abbdab387118ce52f11509564a86b32ff
HanmaDevin <devin.cardosa@proton.me>
2024-11-09 00:59:17 +0100

    updated packages

--- windows/install.ps1
+++ windows/install.ps1
@@ -1,4 +1,4 @@
-$packages = "WhatsApp","Debian.Debian","GNU.Nano","lazygit","python","direnv","KDE.okular","ohmyposh","Valve.steam","Google.chrome","oracle.jdk.23","7-zip"
+$packages = "ajeetdsouza.zoxide","WhatsApp","Debian.Debian","GNU.Nano","lazygit","python","direnv","KDE.okular","ohmyposh","Valve.steam","Google.chrome","oracle.jdk.23","7-zip"
 
 foreach ($element in $packages) {
     winget install $element
